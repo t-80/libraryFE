db58c3d277cd79086f8c42f41c9e5024da6ba7f3
alexandr <t80ttt@gmail.com>
2019-10-27 20:20:03 +0200

    Roles

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import './polyfills';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+  
