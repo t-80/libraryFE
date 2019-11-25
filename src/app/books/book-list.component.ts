697ce3eff144cf2a6b0f20d593dc4749145a0473
alex.garkusha <alex.garkusha@sintez.co.za>
2019-11-25 11:25:34 +0200

    fixes

---
+++ src/app/books/book-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent {
+  title = 'libraryFe';
+}
