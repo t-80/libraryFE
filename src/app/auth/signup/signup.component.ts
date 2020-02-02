552ee1bf3c90df737ed9f5bc3b3f98722597a58f
alexandr <t80ttt@gmail.com>
2020-02-02 20:34:15 +0200

    reactive signup

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MustMatch } from 'src/app/_shared/_helpers/must-match.validator';
 
 @Component({
   selector: 'app-signup',
@@ -7,15 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  isVisible: boolean;
+  options: string[] = ['Here', 'Will', 'Be', 'Options'];
+  signupForm: FormGroup;
+    submitted = false;
 
-  constructor() { }
+    constructor(private formBuilder: FormBuilder) { }
 
-  public changeVisibility() {
-    this.isVisible = !this.isVisible;
-  }
+    ngOnInit() {
+        this.signupForm = this.formBuilder.group({
+            name: ['', Validators.required],
+            options: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            confirmPassword: ['', Validators.required]
+        }, {
+            validator: MustMatch('password', 'confirmPassword')
+        });
+    }
 
-  ngOnInit() {
-  }
+    // convenience getter for easy access to form fields
+    get form() { return this.signupForm.controls; }
 
+    signup() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.signupForm.invalid) {
+            return;
+        }
+
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.signupForm.value))
+    }
 }
